7897b1d99013c39b1cc320f94543822b2c155121
Sahil Slide Sigma <webdevelopersahil@gmail.com>
2025-05-26 15:53:30 +0530

    Hello world test.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here
+import HelloWorldScreen from './HelloWorldScreen';
+import HomeButton from './HomeButton';
+
+export const applyCustomCode = externalCodeSetup => {
+  externalCodeSetup.navigationApi.addNavigationRoute({
+    name: 'HelloWorld',
+    component: HelloWorldScreen,
+    options: {
+      title: 'Hello World',
+    },
+  });
+
+  externalCodeSetup.hooks.addFilter(
+    'app.HomeScreen.render',
+    'custom.home.button',
+    (DefaultComponent) => (props) => (
+      <>
+        <HomeButton />
+        <DefaultComponent {...props} />
+      </>
+    )
+  );
 };
