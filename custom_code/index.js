b5243e8ed1d97f4897cc8e5a5b5c96e672f89e81
Sahil Slide Sigma <webdevelopersahil@gmail.com>
2025-05-28 16:13:28 +0530

    Reset

--- custom_code/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HelloBlock from './HelloBlock';
-
-export const applyCustomCode = (externalCodeSetup) => {
-  externalCodeSetup.blocksApi.addCustomBlockRender(
-    'custom/hello-block',
-    (props) => <HelloBlock {...props} />
-  );
-};
